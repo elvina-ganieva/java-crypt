984ac75f4ce5fea6821d6a7c5bfa7089fd592b65
Elvina Ganieva <elvinaganieva5411@yandex.ru>
2023-04-13 20:14:30 +0300

    hw1

--- hw1/src/main/java/org/example/RandomizerFactory.java
+++ hw1/src/main/java/org/example/RandomizerFactory.java
@@ -7,9 +7,9 @@ public class RandomizerFactory {
 
     static Random getRandomizer(String mode) {
         if (mode.equals("Basic")) {
-            return new Random();
+            return new Random(System.nanoTime());
         } else if (mode.equals("Secure")) {
-            return new SecureRandom();
+            return new SecureRandom(new byte[]{(byte) System.nanoTime()});
         } else {
             throw new IllegalArgumentException("Неизвестный параметр: " + mode);
         }
