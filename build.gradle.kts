db02ab7da55781af6fd7dffc7ab2b41f8670724f
Elvina Ganieva <elvinaganieva5411@yandex.ru>
2023-05-24 21:13:23 +0300

    hw6

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,3 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
-}
